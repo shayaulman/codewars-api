0704e52656ef2d1f2b94b08a2d8ae3cb9d3477fc
Shaye Ulman <shayeulman@gmail.com>
2020-01-28 20:37:08 +0200

    another 🔨

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="app" class="max-w-5xl mx-auto">
-    <header class="mb-24 flex flex-wrap bg-brand-gray-2 rounded-b-lg">
-      <div class="relative p-4 w-2/3 flex flex-col justify-around items-center rounded-b-lg"> 
+    <header class="mb-24 flex bg-brand-gray-2 rounded-b-lg">
+      <div class="relative m-4 p-4 w-full flex flex-col justify-around items-center rounded-b-lg"> 
         <img style="transform:scale(1.3)" class="m-4" :src="`https://www.codewars.com/users/${userName}/badges/large`" alt />
         <h1 class="m-4 text-center text-xl text-brand-gray-4">{{userName === 'Shaya Ulman' ? 'I' : 'you'}} have completed <span class="text-brand-red-1">{{ katas.data.length }} katas</span> so far!</h1>
         <button @mousedown="$confetti.start({defaultDropRate: 20,particlesPerFrame: 5})" @mouseup="$confetti.stop()" class="mt-12 py-2 px-3 absolute bottom-0 left-0  bg-purple-700 rounded-full hover:opacity-75 focus:outline-none">
           🎉
         </button>
       </div>
-      <div class="p-6 w-96 w-1/3 flex flex-col items-center bg-brand-black-1 rounded-r-lg">
+      <div class="p-6 w-96 flex flex-col items-center bg-brand-black-1 rounded-r-lg">
         <h3 class="text-center text-lg text-brand-gray-4">Check your <a class="text-brand-red-1 hover:underline" href="https://codewars.com" target="_blank">codewars </a>stats!</h3>
         <p class="p-2 text-brand-gray-5 text-center font-thin">Type your username, or create a <a class="text-brand-red-1 hover:underline" href="https://www.codewars.com/users/sign_in" target="_blank">codewars account </a>. and don't forget to <a  href="https://www.codewars.com/users/Shaya%20Ulman" class="text-brand-red-1 hover:underline">follow me</a> overthere :) </p>
         <div class="relative m-6">
